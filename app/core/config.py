03b72923f5ce680dd951f84cbe725b966dfd9194
Farah-HnR <farahzahir30@gmail.com>
2026-05-22 15:37:00 +0500

    Initial commit

---
+++ app/core/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    APP_NAME: str = "FinanceBudget"
+    APP_ENV: str = "development"
+    SECRET_KEY: str
+    DATABASE_URL: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
